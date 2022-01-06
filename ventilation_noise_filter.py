7274fe08c93a0bd664241abeaa00bb5b586a6feb
Eric Svärd <eric.svard@gmail.com>
2022-01-06 13:58:47 +0100

    Add outer and inner cylinder and thickness of 1mm

--- ventilation_noise_filter.py
+++ ventilation_noise_filter.py
@@ -6,6 +6,9 @@ from bmesh.types import BMVert
 import numpy as np
 from math import *
 
+bpy.ops.object.select_all(action='DESELECT')
+
+
 # This plugin adds a mesh object which is created based on research paper
 # "Broadband Acoustic Ventilation Barriers". The purpose is to cancel out
 # audible noise in ventilation channels for a specific frequency range.
@@ -92,5 +95,33 @@ bpy.context.view_layer.objects.active = obj
 
 mesh.from_pydata(verts, [], faces)
 
+# Apply scale
 obj.select_set(True)
 bpy.ops.transform.resize(value=(SCALE, SCALE, SCALE))
+bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
+
+# Set thickness for spiral
+bpy.ops.object.modifier_add(type='SOLIDIFY')
+bpy.context.object.modifiers["Solidify"].thickness = 0.001
+bpy.context.object.modifiers["Solidify"].offset = 0
+
+# Add duplicated spiral rotated 180degrees
+bpy.ops.object.duplicate_move()
+bpy.ops.transform.rotate(value=3.14159, orient_axis='Z', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True))
+
+# Join the two spirals
+bpy.data.objects[obj.name].select_set(True)
+bpy.ops.object.join()
+obj_name = bpy.context.selected_objects[0].name
+
+# Add outer cylinder
+bpy.ops.mesh.primitive_cylinder_add(vertices=steps, end_fill_type='NOTHING', radius=D/2/1000, depth=2*h/1000, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+
+bpy.data.objects[obj_name].select_set(True)
+bpy.ops.object.join()
+obj_name = bpy.context.selected_objects[0].name
+
+# Add inner cylinder
+bpy.ops.mesh.primitive_cylinder_add(vertices=steps, end_fill_type='NOTHING', radius=d/2/1000, depth=2*h/1000, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+bpy.data.objects[obj_name].select_set(True)
+bpy.ops.object.join()
